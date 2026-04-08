eb046c1c195daea78159ad67dff1f8431b610bb9
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-08 23:48:38 +0530

    frontend file structure done

---
+++ client/src/pages/CreateRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateRoom = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateRoom
---
+++ client/src/pages/JoinRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JoinRoom = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JoinRoom
